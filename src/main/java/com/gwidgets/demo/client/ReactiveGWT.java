b4c47e5a39aea0ce906f28bda7e657c6c1f445cb
Ignacio Baca Moreno-Torres <ignacio@bacamt.com>
2018-05-11 00:00:48 +0200

    Add fromEvent helper to remove listeners un observable dispose

--- src/main/java/com/gwidgets/demo/client/ReactiveGWT.java
+++ src/main/java/com/gwidgets/demo/client/ReactiveGWT.java
@@ -1,55 +1,51 @@
 package com.gwidgets.demo.client;
 
-
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
 import elemental2.dom.*;
 import io.reactivex.Observable;
 
 import java.util.Objects;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static java.util.stream.Collectors.joining;
 
 public class ReactiveGWT implements EntryPoint {
 
-  HTMLInputElement urlInput = (HTMLInputElement) DomGlobal.document.getElementById("url");
-  HTMLSelectElement methodInput = (HTMLSelectElement) DomGlobal.document.getElementById("method");
-  HTMLTextAreaElement dataInput = (HTMLTextAreaElement) DomGlobal.document.getElementById("data");
-  HTMLElement generatedCommand = (HTMLElement) DomGlobal.document.getElementById("generatedCommand");
-  HTMLInputElement repositoryInput = (HTMLInputElement) DomGlobal.document.getElementById("repository");
-  final String[] commands = new String[3];
+  final HTMLInputElement urlInput = (HTMLInputElement) DomGlobal.document.getElementById("url");
+  final HTMLSelectElement methodInput = (HTMLSelectElement) DomGlobal.document.getElementById("method");
+  final HTMLTextAreaElement dataInput = (HTMLTextAreaElement) DomGlobal.document.getElementById("data");
+  final HTMLElement generatedCommand = (HTMLElement) DomGlobal.document.getElementById("generatedCommand");
 
   public void onModuleLoad() {
+    Observable<Command> methodStream = fromEvent(methodInput, "change")
+        .map(ev -> new Command(1, "-X" + ((HTMLSelectElement) ev.target).value));
 
-    Observable<Command> urlStream = Observable.create((emitter) -> {
-      urlInput.onkeyup = (event) -> {
-        HTMLInputElement urlInputTarget = (HTMLInputElement) event.target;
-        emitter.onNext(new Command(2, urlInputTarget.value));
-        return null;
-      };
-    });
+    Observable<Command> urlStream = fromEvent(urlInput, "keyup")
+        .map(ev -> new Command(2, ((HTMLInputElement) ev.target).value));
 
-    Observable<Command> methodStream = Observable.create((emitter) -> {
-      methodInput.onchange = (event) -> {
-        HTMLSelectElement methodSelect = (HTMLSelectElement) event.target;
-        emitter.onNext(new Command(1, "-X"+methodSelect.value));
-        return null;
-      };
-    });
+    Observable<Command> dataStream = fromEvent(dataInput, "keyup")
+        .map(ev -> new Command(3, "-d '" + ((HTMLTextAreaElement) ev.target).value + "'"));
 
+    Observable.merge(urlStream, methodStream, dataStream)
+        .scan(new String[3], (acc, n) -> { acc[n.position - 1] = n.value; return acc; })
+        .subscribe(n -> generatedCommand.textContent = Stream.of(n).filter(Objects::nonNull).collect(joining(" ")));
+  }
 
-    Observable<Command> dataStream = Observable.create((emitter) -> {
-      dataInput.onkeyup = (event) -> {
-        HTMLTextAreaElement dataInputTarget = (HTMLTextAreaElement) event.target;
-        emitter.onNext(new Command(3, "-d '"+dataInputTarget.value+"'"));
-        return null;
-      };
-    });
+  static class Command {
+    final int position;
+    final String value;
 
-    Observable.merge(urlStream, methodStream, dataStream).subscribe((obs) -> {
-      commands[obs.position - 1] = obs.value;
-      generatedCommand.textContent = String.join(" ", Stream.of(commands).filter(Objects::nonNull).collect(Collectors.toList()));
+    Command(int position, String value) {
+      this.position = position;
+      this.value = value;
+    }
+  }
+
+  static Observable<Event> fromEvent(EventTarget source, String type) {
+    return Observable.create(s -> {
+      EventListener listener = s::onNext;
+      source.addEventListener(type, listener);
+      s.setCancellable(() -> source.removeEventListener(type, listener));
     });
   }
 }
